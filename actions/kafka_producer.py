654bbe538299f06a13e5f5dbcfd622e356d9bd7a
Arthur Sargsyan <artur.s@ucraft.com>
2024-01-30 19:42:13 +0400

    implemented kafka consumer action

---
+++ actions/kafka_producer.py
@@ -0,0 +1,23 @@
+from st2common.runners.base_action import Action
+from confluent_kafka import Producer
+import json
+
+class KafkaProducer(Action):
+    def run(self, topic, message, key=None, headers=None):
+        try:
+            conf = {
+                'client.id': self.config.get("kafka_client_id"),
+                'bootstrap.servers': self.config.get("kafka_bootstrap_servers"),
+                'security.protocol': self.config.get("kafka_security_protocol"),
+                'sasl.mechanism': self.config.get("kafka_sasl_mechanism"),
+                'sasl.username': self.config.get("kafka_sasl_username"),
+                'sasl.password': self.config.get("kafka_sasl_password"),
+                'partitioner': self.config.get("kafka_partitioner")
+            }
+            producer = Producer(conf)
+            producer.produce(topic, value=json.dumps(message).encode('utf-8'), key=key, headers=headers)
+            producer.flush()
+
+        except Exception as e:
+            return f"Error: {str(e)}"
+
