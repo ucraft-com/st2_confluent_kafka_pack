6fce97a35b8e4307bd3a1b79bc995a47175de512
david-kocharyan <david.k@ucraft.com>
2025-01-14 07:44:45 +0000

    update sensor email part

--- sensors/kafka_sensor.py
+++ sensors/kafka_sensor.py
@@ -81,10 +81,9 @@ class KafkaSensor(Sensor):
                         ]
 
                         # Check if the email is in the excluded list or matches the dynamic pattern
-                        if (
+                        if email is None or (
                             email not in excluded_emails
-                            # and not re.match(r"testers\+.*@ucraft\.com", email)
-                            # and not re.match(r"BillingUser-\w+@ucraft\.billing", email)
+                            and not re.match(r"BillingUser-\w+@ucraft\.billing", email)
                         ):
                             self.sensor_service.dispatch(
                                 trigger=trigger, payload=payload
